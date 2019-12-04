9bb501461a6f0deace39b23c6d8734dda0dabe5a
knows1 <info@knows1.com>
2019-12-04 20:47:01 +0100

    render()

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -15,8 +15,12 @@ class LuckyController extends AbstractController
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+      #  return new Response(
+      #      '<html><body>Lucky number: '.$number.'</body></html>'
+    #    );
+
+    return $this->render('lucky/number.html.twig', [
+         'number' => $number,
+     ]);
     }
 }
